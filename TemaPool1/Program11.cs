e0a9b817af760d0f655269b0b370081c6352aaea
Vasile Orbai <vasile930@gmail.com>
2020-11-20 14:20:53 +0200

    add Program11

---
+++ TemaPool1/Program11.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaPool1
+{
+    class Program11
+    {
+        static void Main(string[] args)
+        {
+            // Afisati in ordine inversa cifrele unui numar n. 
+
+            int n;
+            Console.WriteLine("Acest program afisati in ordine inversa cifrele unui numar introdus de la tastatura");
+            Console.WriteLine();
+            Console.Write("Numarul pe care urmeaza sa fie afisat invers este: ");
+            n = int.Parse(Console.ReadLine());
+
+            while (n > 0)
+            {
+                Console.Write(n % 10);
+                n = n / 10;
+            }
+            Console.WriteLine();
+        }
+    }
+}

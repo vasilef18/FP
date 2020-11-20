e7fa60064453712987d6749a1aeafed5c3f2585b
Vasile Orbai <vasile930@gmail.com>
2020-11-20 23:44:39 +0200

    Update Program5.cs

--- TemaPool1/Program5.cs
+++ TemaPool1/Program5.cs
@@ -14,21 +14,21 @@ namespace TemaPool1
             //Cifrele se numara de la dreapta la stanga. 
 
             Console.ForegroundColor = ConsoleColor.Cyan;
-            int numar, k;
-            string N;
+            int k, numar;
             Console.WriteLine("Aceast program afiseaza a k-a cifra de la sfarsitul unui numar");
             Console.WriteLine();
             Console.Write($"Numarul introdus este : ");
             numar = int.Parse(Console.ReadLine());
-            N = Convert.ToString(numar);
             Console.Write("A cata cifra sa fie extrasa ");
             k = int.Parse(Console.ReadLine());
-
-            for (int i = 0; i <N.Count(); i++)
+            //532106
+            for (int i = 1; i < k; i++)
             {
-                if (i == N.Count() - k)
-                    Console.WriteLine($"A {k}-a cifra de la capatul numarului este: {N[i]}");
+                numar = numar / 10;
             }
+            numar = numar % 10;
+            Console.WriteLine($"A {k}-a cifra de la capatul numarului este: {numar}");
+            //Console.WriteLine($"A {k}-a cifra de la capatul numarului este: {numar%10}");     //varianta 2 -> se sterge rand 29+30
         }
     }
 }

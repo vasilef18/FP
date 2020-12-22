751be3ad9f1cf97392f1507398722b2d14981b38
Vasile Orbai <vasile930@gmail.com>
2020-12-22 14:51:39 +0200

    add program 18

--- TemaPool1/Program10.cs
+++ TemaPool1/Program10.cs
@@ -21,6 +21,7 @@ namespace TemaPool1
             for (int i = 2; i <= n/2; i++)
             {
                 if (n % i == 0)
+                    Console.WriteLine(n);
                     ok = 1;
             }
             if (ok == 0)
---
+++ TemaPool1/Program18.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaPool1
+{
+    class Program18
+    {
+        static void Main(string[] args)
+        {
+            //Afisati descompunerea in factori primi ai unui numar n.
+            //De ex. pentru n = 1776 afisati 2^3 x 3^1 x 7^2. 
+
+            int n, m, contor;
+            Console.WriteLine("Acest program afiseaza descompunerea in factori primi ai unui numar");
+            Console.WriteLine();
+            Console.Write("Numarul care urmeaza sa fie descompus este: ");
+            n = int.Parse(Console.ReadLine());
+            m = n;
+
+            for (int divizor = 2;  divizor <= n / 2; divizor++)
+            {
+                if (m % divizor == 0)
+                {
+                    contor = 0;
+                    while (m % divizor == 0)
+                    {
+                        contor++;
+                        m = m / divizor;
+                    }
+                    Console.Write($"{divizor}^{contor}*");
+                }
+
+                if (m == 1)
+                    break;
+            }
+            Console.WriteLine("1");
+        }
+    }
+}

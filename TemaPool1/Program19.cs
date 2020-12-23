439016a9e1b1d4925f2adc79685f83ae4149a0c9
Vasile Orbai <vasile930@gmail.com>
2020-12-24 00:30:47 +0200

    Add program19, started program20

---
+++ TemaPool1/Program19.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace TemaPool1
+{
+    class Program19
+    {
+        static void Main(string[] args)
+        {
+            //Determinati daca un numar e format doar cu 2 cifre care se pot repeta.
+            //De ex. 23222 sau 9009000 sunt astfel de numere, pe cand 593 si 4022 nu sunt. 
+
+            Console.WriteLine("Acest program determina daca un numar este format doar din 2 cifre care se pot repeta");
+            Console.WriteLine("De ex. 23222 sau 9009000 sunt astfel de numere, pe cand 593 si 4022 nu sunt");
+            Console.WriteLine();
+
+            int n , cifra1=0 , cifra2=-1, cifra3=0;
+            Console.Write("Numarul care urmeaza sa fie verificat este: ");
+            n = int.Parse(Console.ReadLine());
+
+            if (n < 9)
+            {
+                Console.WriteLine("Numarul introdus este format doar dintr-o cifra distincta");
+            }
+            else
+            {
+                cifra1 = n % 10;
+                n = n / 10;
+
+                cifra2 = n % 10;
+                n = n / 10;
+                while (cifra1 == cifra2 && n > 0)
+                {
+                    cifra2 = n % 10;
+                    n = n / 10;
+                }
+
+                cifra3 = n % 10;
+                n = n / 10;
+
+                while ((cifra2 == cifra3 || cifra1 == cifra3) && n > 0)
+                {
+                    cifra3 = n % 10;
+                    n = n / 10;
+                }
+
+                if (cifra1 != cifra2 && cifra1 != cifra3 && cifra2 != cifra3)
+                {
+                    Console.WriteLine("Numarul introdus NU este format doar din 2 cifre distincte");
+                }
+                else
+                {
+                    Console.WriteLine("Numarul introdus este format doar din 2 cifre distincte");
+                }
+            }
+        }
+    }
+}
---
+++ TemaPool1/Program20.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaPool1
+{
+    class Program20
+    {
+        static void Main(string[] args)
+        {
+            //Afisati fractia m / n in format zecimal, cu perioada intre paranteze(daca e cazul).Exemplu: 13 / 30 = 0.4(3).
+
+            int m, n;
+            double rezultat;
+            Console.WriteLine("Acest program afiseaza fractia m/n in format zecimal");
+            Console.WriteLine();
+            Console.Write("m=");
+            m = int.Parse(Console.ReadLine());
+            Console.Write("n=");
+            n = int.Parse(Console.ReadLine());
+
+            rezultat = m / n;
+            Console.WriteLine($"Rezultatul fractie {m}/{n} este {rezultat}");
+        }
+    }
+}

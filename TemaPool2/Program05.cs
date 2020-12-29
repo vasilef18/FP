8a3d372e912cc9ba1a376855fdbd42a0f60834fd
Vasile Orbai <vasile930@gmail.com>
2020-12-29 17:22:59 +0200

    Add Program05

---
+++ TemaPool2/Program05.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaPool2
+{
+    class Program05
+    {
+        static void Main(string[] args)
+        {
+            //Cate elemente dintr-o secventa de n numere sunt egale cu pozitia pe care apar in secventa.
+            //Se considera ca primul element din secventa este pe pozitia 0.
+
+            int lungimeSecventa, numar=0, pozitie=0;
+            Random aleator = new Random();
+            Console.WriteLine("Programul afiseaza cate dintre elementele unei secvente sunt egale cu pozitia pe care apar");
+            Console.WriteLine();
+            Console.Write("Introduceti lungimea secventei: ");
+            lungimeSecventa = int.Parse(Console.ReadLine());
+
+            Console.Write("Elementele secventei sunt: ");
+            string save="";
+            for (int i = 0; i <= lungimeSecventa; i++)
+            {
+                numar = aleator.Next(lungimeSecventa);
+                Console.Write($"{numar}, ");
+                if (numar == i)
+                {
+                    save += $"\nElementul {numar} se afla pe pozitia {i}";
+                    //Console.WriteLine();
+                    //Console.WriteLine($"Elementul {numar} se afla pe pozitia {i}");
+                }
+            }
+            Console.WriteLine();
+            Console.WriteLine(save);
+        }
+
+    }
+}

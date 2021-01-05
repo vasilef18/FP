525ca8ed7201444191ee3af3e9e7cdb3d8e841c9
Vasile Orbai <vasile930@gmail.com>
2021-01-05 18:54:48 +0200

    Add TemaPool3 , program01, program02

---
+++ TemaPool3/Program01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaPool3
+{
+    class Program01
+    {
+        static void Main(string[] args)
+        {
+            //Calculati suma elementelor unui vector de n numere citite de la tastatura. 
+            //Rezultatul se va afisa pe ecran.
+
+            int n, i, suma = 0;
+            Console.WriteLine("Programul calculeaza suma elementelor unui vector de n numere");
+            Console.WriteLine();
+            Console.Write("Numarul elementelor vectorului este: ");
+            n = int.Parse(Console.ReadLine());
+            int[] vector = new int[n];
+
+            Console.WriteLine("Elementele vectorului sunt: ");
+            for (i = 0; i < n; i++)
+            {
+                vector[i] = int.Parse(Console.ReadLine());
+                suma += vector[i];
+            }
+            Console.WriteLine($"Suma elementelor este: {suma}");
+        }
+    }
+}
---
+++ TemaPool3/Program02.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TemaPool3
+{
+    class Program02
+    {
+        static void Main(string[] args)
+        {
+            //Se da un vector cu n elemente si o valoare k. Se cere sa se determine 
+            //prima pozitie din vector pe care apare k. Daca k nu apare in vector rezultatul va fi -1. 
+
+            int n, k, i, ok = 0, pozitie = 0;
+            Random aleator = new Random();
+            Console.WriteLine("Programul determina prima pozitie pe care apare in vector un element" +
+                " k citit de la tastatura.");
+            Console.WriteLine();
+            Console.Write("Dati numarul de elemente al vectorului: ");
+            n = int.Parse(Console.ReadLine());
+            Console.Write($"Elementul cautat in vector este: ");
+            k = int.Parse(Console.ReadLine());
+            int[] vector = new int[n];
+
+            Console.Write("Elementele vectorului sunt: ");
+            for (i = 0; i < n; i++)
+            {
+                vector[i] = aleator.Next(0, 5);
+                Console.Write($"{vector[i]} ");
+                if (vector[i] == k)
+                {
+                    ok = 1;
+                    pozitie = i;
+                    break;
+                }
+            }
+            Console.WriteLine();
+            if (ok == 1)
+            {
+                Console.WriteLine($"Elementul {k} apare prima data in vector pe pozitia {pozitie}");
+            }
+            else
+            {
+                Console.WriteLine("-1");
+            }
+        }
+    }
+}
